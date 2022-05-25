ecb14b3868017a009a9cf8e8364fdc3b5fa1f4e7
bobby2510 <kvp.babby@gmail.com>
2022-05-25 11:57:56 +0530

    new links

--- src/App.js
+++ src/App.js
@@ -70,11 +70,18 @@ const App = ()=>{
     }
     const backendList = [
     //    'http://localhost:5000'
-        'https://tg-node-eight.herokuapp.com',
-        'https://tg-node-nine.herokuapp.com',
-        'https://tg-node-ten.herokuapp.com',
-        'https://tg-node-eleven.herokuapp.com',
-        'https://tg-node-twelve.herokuapp.com',
+    'https://tg-node-eight.herokuapp.com',
+    'https://tg-node-nine.herokuapp.com',
+    'https://tg-node-ten.herokuapp.com',
+    'https://tg-node-eleven.herokuapp.com',
+    'https://tg-node-twelve.herokuapp.com',
+    'https://tg-node-extra-one.herokuapp.com',
+    'https://tg-node-extra-two.herokuapp.com',
+    'https://tg-node-extra-three.herokuapp.com',
+    'https://tg-node-extra-four.herokuapp.com',
+    'https://tg-node-extra-five.herokuapp.com',
+    'https://tg-node-extra-six.herokuapp.com',
+    'https://tg-node-extra-seven.herokuapp.com'
     ]
     const [reload, setReload] = useState(null)
     const [sportIndex,setSportIndex] = useState(0) // change 
